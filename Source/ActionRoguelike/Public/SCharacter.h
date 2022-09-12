2ab58b95e877209483fc60e5a4908149aa4b9149
Carl Altoveros <caaltoveros@riotgames.com>
2022-09-11 22:34:18 -0700

    Generating project and adding SCharacter & Setup Input

---
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent; 
+
+UCLASS()
+class ACTIONROGUELIKE_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Value is a float from -1 to 1 where negative values move a character back on an axis and positive values move a character forward on an axis
+	void MoveForward(float Value);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
